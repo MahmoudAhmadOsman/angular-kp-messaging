9759554598cea1f1eae58c3fc97a0561ce460e97
Mahmoud Osman <mahmoudosman.som@gmail.com>
2021-04-11 12:25:32 -0500

    added footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  year: Date = new Date();
+  public year: Date = new Date();
   constructor() { }
 
   ngOnInit(): void {
